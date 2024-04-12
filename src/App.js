22ba6161ab640ae589d9608c80de02df312765dc
melaradev <oscar.melara.dev@gmail.com>
2024-04-12 15:21:56 -0600

    New files directory

--- src/App.js
+++ src/App.js
@@ -1,27 +1,30 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Router, Switch } from "react-router-dom";
+
 //import HomePage from "./Components/HomePage/HomePage";
-import Menu from "./Components/MenuContainer/Menu/Menu";
-import Header from "./Components/Header/Header";
-import Body from "./Components/Body/BodySection";
 import "./App.css";
+import AppRouter from "./router/AppRouter";
 
 //Styles
 function App() {
   return (
     <div className="app-container">
-   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"></link>
-      <BrowserRouter>
+      <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous"
+      ></link>
+      <AppRouter />
+      {/*<BrowserRouter>
       <Menu />
         <Routes>
           <Route path="/" element={<>
            <Header/>
            <Body/>
-           {/*<HomePage />*/}
+           <HomePage 
           </>} />
         </Routes>
-      </BrowserRouter>
- 
+      </BrowserRouter>/>*/}
     </div>
   );
 }
---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../../Components/Header/Header"
+import Body from "../../Components/Body/BodySection"
+const HomePage = () => {
+    return (
+        <>
+        <Header/>
+        <Body/>
+        </>
+        
+    )
+}
+
+export default HomePage
